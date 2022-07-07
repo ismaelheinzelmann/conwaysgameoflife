e2c367acc8a6d09920f2582a9d0893b9d01c0d6c
Ismael Heinzelmann <ismaelheinzelmann@gmail.com>
2022-07-07 09:05:15 -0300

    some conways class and interface updates

--- ConwaysGame.py
+++ ConwaysGame.py
@@ -1,26 +1,45 @@
 import pygame
+from pygame.locals import K_ESCAPE
 import sys
-from grid import Grid
-
+from Grid import Grid
 from colors import blue, red, orange, yellow, cream, black
-
 display = pygame.display.set_mode((1400, 900))
 
 class ConwaysGame():
 
     def __init__(self):
+        self.running = True
+        self.counter = 0
+        self.tickrate = 60
+    
+    def input_handler(self, event):
+        if event.type == pygame.QUIT:
+            sys.exit(0)
+        elif event.type == pygame.KEYDOWN:
+            if event.key == K_ESCAPE:
+                self.running = False
+    
+    def click_handler(self, event):
         pass
     
-    def input_handler(self, args):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit(0)
+    def set_tickrate(self, tickrate):
+        self.tickrate = tickrate
+        self.counter = 0
+    
+    def increase_tick(self, world):
+        if self.counter < self.tickrate:
+            self.counter += 1
+        else:
+            self.counter = 0
+            world.update_grid()
+            
 
     def draw_dead(self, display, i, j, blocksize):
-        pygame.draw.rect(display, blue, (blocksize*i, blocksize*j, blocksize, blocksize))
+        pygame.draw.rect(display, blue, (blocksize*i, blocksize*j, blocksize, blocksize), 2)
 
     def draw_alive(self, display, i, j,blocksize):
-        pygame.draw.rect(display, orange, (blocksize*i, blocksize*j, blocksize,blocksize))
+        pygame.draw.rect(display, blue, (blocksize*i, blocksize*j, blocksize, blocksize), 2)
+        pygame.draw.rect(display, orange, ((blocksize*i)+2, (blocksize*j)+2, blocksize-2 ,blocksize-2 ))
 
     def main(self):
         pygame.init()
@@ -56,12 +75,14 @@ class ConwaysGame():
                         [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
                         [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
                         [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]])
-        running = True
 
-        while running:
+        while self.running:
+            #Event handling in the tick start
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    running = False
+                    self.running = False
+                elif event.type == pygame.KEYDOWN:
+                    self.input_handler(event)
 
 
             clock = pygame.time.Clock()
@@ -73,9 +94,10 @@ class ConwaysGame():
                     else: self.draw_dead(display, j, i, size)
 
 
-            clock.tick(1)
-            world.update_grid()
+            clock.tick(60)
+            self.increase_tick(world)
             pygame.display.flip()
+        pygame.quit()
 
 game = ConwaysGame()
 game.main()
