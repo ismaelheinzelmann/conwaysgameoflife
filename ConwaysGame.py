3df6d78342593c09b37466b943e9563283b087d4
Ismael Heinzelmann <ismaelheinzelmann@gmail.com>
2022-07-07 13:18:30 -0300

    edit feature implemented but need to work on tick speed

--- ConwaysGame.py
+++ ConwaysGame.py
@@ -12,47 +12,49 @@ class ConwaysGame():
         self.running = True
         self.counter = 0
         self.tickrate = 60
-        self.tickaux = 1
-        self.state = State("game")
-        self.display = pygame.display.set_mode((1400, 900))
-        self.world = Grid(30)
+        self.tickaux = 10
+        self.state = State("creating")
+        self.display = pygame.display.set_mode((1500, 1000))
+        self.n = 50
+        self.world = Grid(self.n)
+        self.size = 20
     
     def input_handler(self, event):
         if event.key == K_ESCAPE:
             self.running = False
-        elif event.key == K_z:
-            self.set_tickrate(-1)
         elif event.key == K_x:
+            self.set_tickrate(-1)
+        elif event.key == K_z:
             self.set_tickrate(1)
         elif event.key == K_e:
             self.state.set_creating()
         elif event.key == K_SPACE:
             self.state.set_evolving()
         elif event.key == K_r:
-            self.world.reset_grid(30)
+            self.state.set_creating()
+            self.world.reset_grid(self.n)
         elif event.key == K_h: self.state.switch_help()
 
     def click_handler(self):
         pos = pygame.mouse.get_pos()
-        x, y = (floor(pos[0]/30),floor(pos[1]/30))
-        if x < 30 and y < 30:
-            print(x, y)
+        x, y = (floor(pos[0]/self.size),floor(pos[1]/self.size))
+        if x < self.n and y < self.n:
             self.world.switch_cell(y,x)
             pygame.display.flip()
     
     def set_tickrate(self, tick):
         s = self.tickaux+tick
-        if s > 0 and s < 9:
+        if s > 0 and s < 11:
             self.counter = 0
             self.tickaux += tick
-            self.tickrate = floor(60/(s))
+            self.tickrate += 6*tick
     
     def increase_tick(self, world):
         if self.counter < self.tickrate:
             self.counter += 1
         else:
             self.counter = 0
-            world.update_grid()            
+            world.update_grid()
 
     def draw_dead(self, display, i, j, blocksize):
         pygame.draw.rect(display, blue, (blocksize*i, blocksize*j, blocksize, blocksize), 2)
@@ -60,48 +62,18 @@ class ConwaysGame():
     def render_grid(self, world):
         for i in range(len(world.grid)):
                 for j in range(len(world.grid)):
-                    size = 30
-                    if world.grid[i][j] == 1: self.draw_alive(self.display, j, i, size)
-                    else: self.draw_dead(self.display, j, i, size)
+                    if world.grid[i][j] == 1: self.draw_alive(self.display, j, i, self.size)
+                    else: self.draw_dead(self.display, j, i, self.size)
 
     def draw_alive(self, display, i, j,blocksize):
         pygame.draw.rect(display, blue, (blocksize*i, blocksize*j, blocksize, blocksize), 2)
         pygame.draw.rect(display, orange, ((blocksize*i)+2, (blocksize*j)+2, blocksize-2 ,blocksize-2 ))
 
+    #main game routine
     def main(self):
         pygame.init()
         pygame.display.set_caption("Conway's Game Of Life by Ismael Heinzelmann")
         self.set_tickrate(1)
-        self.world.set_grid([[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0],
-                            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0],
-                            [0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0],
-                            [0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0],
-                            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,0,0,0,0,0,0,0],
-                            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,0,0,0,0,0,0,0],
-                            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]])
 
         while self.running:
             clock = pygame.time.Clock()
